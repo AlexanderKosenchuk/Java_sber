8699bded9fc360e73584df1879268b315cba362a
AlexanderKosenchuk <Venturax@mail.ru>
2020-06-18 18:51:30 +0500

    Задание 1 задача номер 2

--- OneTwo.java
+++ OneTwo.java
@@ -1,8 +1,8 @@
 class OneTwo {
-    private int[] arr;
+     int[] arr;
 
 
-    public OneTwo(int arr[]) {
+    public OneTwo(int [] arr) {
         this.arr = arr;
 
     }
@@ -10,9 +10,9 @@ class OneTwo {
     public void add(int x) {
         int[] msv1 = new int[arr.length + 1];
         for (int i = 0; i < arr.length ; i++) {
-            arr[i] = msv1[i];
+            msv1[i] = arr[i];
         }
-        msv1[arr.length - 1] = x;
+        msv1[arr.length] = x;
     }
 
     public void print() {
@@ -26,6 +26,38 @@ class OneTwo {
         return arr.length;
     }
 
+    public void deleteElement(int index) {
+        int [] msvDel = new int [arr.length - 1];
+        for (int i = 0; i < index; i++) {
+            msvDel[i] = arr[i];
+        }
+        for (int i = index + 1; i < arr.length; i++) {
+            msvDel[i - 1] = arr[i];
+        }
+
+
+
+    }
+
+    public int minimal(){
+        int min = arr[0];
+        for (int i = 0; i < arr.length ; i++) {
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public int maximal(){
+        int max = arr[0];
+        for (int i = 0; i < arr.length ; i++) {
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 
 }
 
