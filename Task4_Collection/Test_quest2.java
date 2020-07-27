e382ee65ead3fe9ed134c440295164d066c8db00
AlexanderKosenchuk <Venturax@mail.ru>
2020-07-27 18:22:00 +0500

    Задание 4 - счетчик букв(считать с клавы)

--- Task4_Collection/Test_quest2.java
+++ Task4_Collection/Test_quest2.java
@@ -1,9 +1,11 @@
 package Task4_Collection;
 import java.util.*;
+import java.io.*;
 
 public class Test_quest2 {
-    public static void main(String[] args) {
-        String text = "On the Atlantic shore of the United States, much of the northern coast is rocky. But the middle and southern Atlantic coasts rise gently from the sea. The Appalachians, which run almost parallel to the east coast, are old mountains with many coal rich valleys between them To the west of the Appalachians, lie plateaus built up over the centuries from bits of stone that were washed down from the mountains and then cut into small hills by streams.";
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // это если ввести свой текст руками
+        String text = reader.readLine();  //"On the Atlantic shore of the United States, much of the northern coast is rocky. But the middle and southern Atlantic coasts rise gently from the sea. The Appalachians, which run almost parallel to the east coast, are old mountains with many coal rich valleys between them To the west of the Appalachians, lie plateaus built up over the centuries from bits of stone that were washed down from the mountains and then cut into small hills by streams.";
 
         Map<Character, Integer> frequency = new TreeMap<>();
 
